d79218b4bc113483a815fc0a4d4bf274cec3ce1c
Corragon <corragon@gmail.com>
2014-12-07 17:47:40 -0600

    Fixed slight color difference in plot1

--- plot1.R
+++ plot1.R
@@ -2,5 +2,8 @@ plot1 <- function() {
   source("load_power_consumption.R")
   power <- load()
   
-  hist(power$Global_active_power, main="Global Active Power", col="Red", xlab="Global Active Power (kilowatts)")
+  hist(power$Global_active_power, 
+       main="Global Active Power", 
+       col=rgb(255, 37, 0, max=255), 
+       xlab="Global Active Power (kilowatts)")
 }
